320d24ad599e3efd52620caa696662dd805505d5
monkeyMax-SVG <monkeymax@yeah.net>
2021-02-27 10:01:15 +0800

    some change

--- google_model_viewer/script.js
+++ google_model_viewer/script.js
@@ -32,34 +32,13 @@ function loadPlaces(position) {
 
 
 /**
- * converts a XYZ vector3 to longitude latitude (Direct Polar)
- * @param lng longitude
- * @param lat latitude
- * @param vector3 optional output vector3
- * @returns a unit vector of the 3d position
- */
-function lonLatToVector3( lng, lat )
-{
-    //flips the Y axis
-    lat = Math.PI / 2 - lat;
-
-    return [
-        Math.sin( lat ) * Math.sin( lng ),
-        Math.cos( lat ),
-        Math.sin( lat ) * Math.cos( lng )
-    ];
-
-}
-
-
-/**
  * converts a XYZ THREE.Vector3 to longitude latitude. beware, the vector3 will be normalized!
  * @param vector3 
  * @returns an array containing the longitude [0] & the lattitude [1] of the Vector3
  */
 function vector3toLonLat( vector3 )
 {
-    console.log(vector3)
+
     // vector3.normalize();
 
     //longitude = angle of the vector around the Y axis
@@ -87,6 +66,19 @@ function vector3toLonLat( vector3 )
 
 }
 
+function calcPosFromLatLonRad(lat,lon){
+    const radius = 6371
+    var phi   = (90-lat)*(Math.PI/180)
+    var theta = (lon+180)*(Math.PI/180)
+
+    x = -((radius) * Math.sin(phi)*Math.cos(theta))
+    z = ((radius) * Math.sin(phi)*Math.sin(theta))
+    y = ((radius) * Math.cos(phi))
+  
+    return [x,y,z]
+
+}
+
 window.onload = () => {
     const scene = document.querySelector('a-scene');
 
@@ -94,7 +86,7 @@ window.onload = () => {
     return navigator.geolocation.getCurrentPosition(function (position) {
         console.log(position.coords.latitude + " " + position.coords.longitude)
         
-        var val = lonLatToVector3(position.coords.longitude, position.coords.latitude)
+        var val = calcPosFromLatLonRad(position.coords.longitude, position.coords.latitude)
         console.log(val)
         
         var vll = vector3toLonLat((new THREE.Vector3(val[0], val[1], val[2])))
@@ -129,3 +121,52 @@ window.onload = () => {
         }
     );
 };
+
+//:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+//:::                                                                         :::
+//:::  This routine calculates the distance between two points (given the     :::
+//:::  latitude/longitude of those points). It is being used to calculate     :::
+//:::  the distance between two locations using GeoDataSource (TM) prodducts  :::
+//:::                                                                         :::
+//:::  Definitions:                                                           :::
+//:::    South latitudes are negative, east longitudes are positive           :::
+//:::                                                                         :::
+//:::  Passed to function:                                                    :::
+//:::    lat1, lon1 = Latitude and Longitude of point 1 (in decimal degrees)  :::
+//:::    lat2, lon2 = Latitude and Longitude of point 2 (in decimal degrees)  :::
+//:::    unit = the unit you desire for results                               :::
+//:::           where: 'M' is statute miles (default)                         :::
+//:::                  'K' is kilometers                                      :::
+//:::                  'N' is nautical miles                                  :::
+//:::                                                                         :::
+//:::  Worldwide cities and other features databases with latitude longitude  :::
+//:::  are available at https://www.geodatasource.com                         :::
+//:::                                                                         :::
+//:::  For enquiries, please contact sales@geodatasource.com                  :::
+//:::                                                                         :::
+//:::  Official Web site: https://www.geodatasource.com                       :::
+//:::                                                                         :::
+//:::               GeoDataSource.com (C) All Rights Reserved 2018            :::
+//:::                                                                         :::
+//:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+function distance(lat1, lon1, lat2, lon2, unit) {
+	if ((lat1 == lat2) && (lon1 == lon2)) {
+		return 0;
+	}
+	else {
+		var radlat1 = Math.PI * lat1/180;
+		var radlat2 = Math.PI * lat2/180;
+		var theta = lon1-lon2;
+		var radtheta = Math.PI * theta/180;
+		var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+		if (dist > 1) {
+			dist = 1;
+		}
+		dist = Math.acos(dist);
+		dist = dist * 180/Math.PI;
+		dist = dist * 60 * 1.1515;
+		if (unit=="K") { dist = dist * 1.609344 }
+		if (unit=="N") { dist = dist * 0.8684 }
+		return dist;
+	}
+}
